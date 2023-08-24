7cf4b7e422dc1b6b5c184cb0621ecfa11bddf206
Marsh1100 <margie.b.agu17@gmail.com>
2023-08-24 17:18:25 -0500

    Controladores

---
+++ store/APIstore/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIstore.Controllers;
+[ApiController]
+[Route("api/[controller]")] //Tomará el nombre de la clase
+public class BaseApiController : ControllerBase //Clase propia del entity
+{
+        
+}
---
+++ store/APIstore/Controllers/CountryController.cs
@@ -0,0 +1,7 @@
+
+namespace APIstore.Controllers;
+
+public class CountryController : BaseApiController
+{
+    
+}
